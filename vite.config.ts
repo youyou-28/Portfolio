9dc5529edfb93a8455f1a4e1e53713e788f40a89
Younes Nedjar <147207701+youyou-28@users.noreply.github.com>
2025-01-15 20:58:00 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
+  }
 });
