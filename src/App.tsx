9e1e7f2e8ac6bde81afee27ac141b81db96ba80d
Younes Nedjar <147207701+youyou-28@users.noreply.github.com>
2025-06-03 23:33:12 +0200

    Ajouter la page des apprentissages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { ProjectPage } from './pages/ProjectPage';
+import { LearningPage } from './pages/LearningPage';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/project/:slug" element={<ProjectPage />} />
+          <Route path="/learning" element={<LearningPage />} />
         </Routes>
       </div>
     </Router>
